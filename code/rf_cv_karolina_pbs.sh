ec71b54b545aed6bb6569550150dfdb24c8906ab
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-03-20 18:27:18 +0100

    zmena paralelizace plus zakomentovani odevzdavani

--- code/rf_cv_karolina_pbs.sh
+++ code/rf_cv_karolina_pbs.sh
@@ -5,15 +5,15 @@
 #PBS -e rf_cv.e
 #PBS -o rf_cv.o
 
-cd ~/KPMS-IT4I-EX/code
+cd ~/KPMS-IT4I-EX-1/code
 pwd
 
 module load R
 echo "loaded R"
 
-time Rscript rf_cv_serial.r
-time Rscript rf_cv_mc.r 8
-time Rscript rf_cv_mc.r 16
+#time Rscript rf_cv_serial.r
+#time Rscript rf_cv_mc.r 8
+#time Rscript rf_cv_mc.r 16
 time Rscript rf_cv_mc.r 32
 time Rscript rf_cv_mc.r 64
 time Rscript rf_cv_mc.r 128
