057b97eed690d814e153665a8b9c81fe4cea56d6
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-19 21:30:25 +0200

    zakomentovani kodu

--- mpi/mnist_svd_cv.R
+++ mpi/mnist_svd_cv.R
@@ -103,6 +103,40 @@ pars = seq(80.0, 95, 5)      ## par values to fit
 folds = sample( rep_len(1:nfolds, nrow(train)), nrow(train) ) ## random folds
 cv = expand.grid(par = pars, fold = 1:nfolds)  ## all combinations
 
+
+#------------------------------------------------------------------------
+#jara zkousi programovat cv
+
+#comm.set.seed(seed = 123, diff = TRUE)
+
+#n = nrow(train)
+#n_test = nrow(test)
+#my_trees = comm.chunk(512)
+#my_test_rows = comm.chunk(nrow(test), form = "vector")
+
+#my_rf = randomForest(train, y = train_lab, ntree = my_trees, norm.votes = FALSE)
+#all_rf = allgather(my_rf)
+#all_rf = do.call(combine, all_rf)
+
+#my_pred = as.vector(predict(all_rf, test[my_test_rows, ]))
+
+#correct = reduce(sum(my_pred == test_lab[my_test_rows]))
+#comm.cat("Proportion Correct:", correct/n_test, "\n")
+
+#finalize()
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------------------------------------------
+
+
 ## function for parameter combination i
 fold_err = function(i, cv, folds, train) {
   par = cv[i, "par"]
