b0d30b32bab2824009beef5c98afcba29c9d9d71
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-20 18:49:56 +0200

    comm rank1 vykresluje

--- mpi/mnist_svd_cv.R
+++ mpi/mnist_svd_cv.R
@@ -180,7 +180,7 @@ comm.print(cv_err_par_colect)
 ## End CV
 
 ## recompute with optimal pct
-if(comm.rank() == 0) { models = svdmod(train, train_lab, pct = 85)
+if(comm.rank() == 1) { models = svdmod(train, train_lab, pct = 85)
 pdf("BasisImages.pdf")
 model_report(models, kplot = 9)
 dev.off()
