b45ea774759e0a7163c46f746afab9dea2c5ea40
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-20 12:43:05 +0200

    2sloty

--- mpi/mnist_svd_cv.sh
+++ mpi/mnist_svd_cv.sh
@@ -18,4 +18,4 @@ export RDMAV_FORK_SAFE=1
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
 
 ## --args blas fork
-time mpirun -np 8 Rscript mnist_svd_cv.R 
+time mpirun -np 2 Rscript mnist_svd_cv.R 
