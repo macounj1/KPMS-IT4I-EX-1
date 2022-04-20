a7e7a7952f8174657a52159574e1b5d530188680
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-20 13:59:34 +0200

    mpirun

--- mpi/mnist_svd_cv.sh
+++ mpi/mnist_svd_cv.sh
@@ -18,4 +18,6 @@ export RDMAV_FORK_SAFE=1
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
 
 ## --args blas fork
-time mpirun -np 3 Rscript mnist_svd_cv.r
+
+localhost slots=25
+time mpirun --hostfile hostfile -np 25 Rscript mnist_svd_cv.r
