06d66485a12d6af2a16fd86fc44d3c68f7c4185d
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-20 14:11:54 +0200

    zmena pripony

--- mpi/mnist_svd_cv.sh
+++ mpi/mnist_svd_cv.sh
@@ -17,4 +17,4 @@ module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
 ## --args blas fork
 
 
-time mpirun  -np 8 Rscript mnist_svd_cv.r
+time mpirun  -np 8 Rscript mnist_svd_cv.R
