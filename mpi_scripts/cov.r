7993bf1bb09693dfc24cb809fa90476f0fa77618
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-20 08:13:18 +0200

    zkousim

--- mpi_scripts/cov.r
+++ mpi_scripts/cov.r
@@ -7,6 +7,7 @@ my.X <- matrix( rnorm(10*3), ncol = 3 )
 comm.print( my.X )
 ## Compute mean
 N <- allreduce( nrow( my.X ), op = "sum" )
+comm.print( N )
 mu <- allreduce( colSums( my.X ) / N, op = "sum" )
 comm.print( mu )
 
