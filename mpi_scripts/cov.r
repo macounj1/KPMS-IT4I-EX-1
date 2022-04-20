cf069f376a89f5f95eb24b43003c7ef9562694db
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-20 08:11:05 +0200

    moje zkouseni

--- mpi_scripts/cov.r
+++ mpi_scripts/cov.r
@@ -8,9 +8,11 @@ my.X <- matrix( rnorm(10*3), ncol = 3 )
 ## Compute mean
 N <- allreduce( nrow( my.X ), op = "sum" )
 mu <- allreduce( colSums( my.X ) / N, op = "sum" )
+comm.print( mu )
 
 ## Sweep out mean and compute crossproducts sum
 my.X <- sweep( my.X, STATS = mu, MARGIN = 2 )
+comm.print( my.X )
 Cov.X <- allreduce( crossprod( my.X ), op = "sum" ) / ( N - 1 )
 
 comm.print( Cov.X )
