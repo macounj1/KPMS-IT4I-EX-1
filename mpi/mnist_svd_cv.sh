d0c48d82a9c719d4881a93195dbbf6898d3cebbd
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-20 14:37:09 +0200

    2 vlakna

--- mpi/mnist_svd_cv.sh
+++ mpi/mnist_svd_cv.sh
@@ -18,4 +18,4 @@ module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
 ## --args blas fork
 
 
-time mpirun --map-by ppr:8:node Rscript mnist_svd_cv.R
+time mpirun --map-by ppr:2:node Rscript mnist_svd_cv.R
