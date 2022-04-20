7ece7b25ff9b11ea3e43d0b5d6c38486705b6407
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-20 16:27:24 +0200

    zvetseni parametru

--- mpi/mnist_svd_cv.R
+++ mpi/mnist_svd_cv.R
@@ -100,8 +100,8 @@ suppressMessages(library(pbdMPI))
 
 
 nfolds = 2
-pars = seq(80.0, 95, 5) ## par values to fit
-index_pars=comm.chunk(length( seq(80.0, 95, 5)), form = "vector")
+pars = seq(80.0, 95, 1) ## par values to fit
+index_pars=comm.chunk(length( seq(80.0, 95, 1)), form = "vector")
 comm.cat( index_pars,"indexpars", "\n",all.rank=TRUE)
 my.rank <- comm.rank()
 
@@ -116,7 +116,7 @@ ranks = comm.size()
 #cat(msg, "\n")
 
 
-k <- comm.chunk(length( seq(80.0, 95, 5)))
+k <- comm.chunk(length( seq(80.0, 95, 1)))
 comm.cat( my.rank, "my rank is:", k, "\n", all.rank = TRUE)
 
 #------------------------------------------------------------------------
