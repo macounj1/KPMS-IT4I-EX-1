704478903eb131b08e32b29e0a50b874c3d55c66
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-21 09:09:29 +0200

    upraveni mnist_svd_mv.r a sh

--- mnist/mnist_svd_mv.sh
+++ mnist/mnist_svd_mv.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-#PBS -N mnist_svd_mv
-#PBS -l select=1:ncpus=128,walltime=00:50:00
+#PBS -N mnist_svd_cv
+#PBS -l select=2:mpiprocs=64,walltime=00:12:00
 #PBS -q qexp
-#PBS -e mnist_svd_mv.e
-#PBS -o mnist_svd_mv.o
+#PBS -e mnist_svd_cv.e
+#PBS -o mnist_svd_cv.o
 
-cd ~/KPMS-IT4I-EX/mnist
+cd ~/KPMS-IT4I-EX/mpi
 pwd
 
 module load R
 echo "loaded R"
+export OMPI_MCA_mpi_warn_on_fork=0
+export RDMAV_FORK_SAFE=1
 
-time Rscript mnist_svd_mv.R
+module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
+
+## --args blas fork
+
+
+time mpirun --map-by ppr:32:node Rscript mnist_svd_mv.R
 
 
